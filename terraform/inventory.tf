8d127167d2376c5f653a0c6502a3ca3d55c55bb1
Ho <hofookeng.uca@gmail.com>
2026-08-26 06:08:59 +0800

    fix: inventory.tf module references

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,8 +1,8 @@
 resource "local_file" "inventory" {
   filename = "inventory.ini"
   content = templatefile("inventory.ini.tftpl", {
-    node1_ip = module.node1.public_ip
-    node2_ip = module.node2.public_ip
-    node3_ip = module.node3.public_ip
+    node1_ip = module.pub-web.public_ip
+    node2_ip = module.pri-ctr.private_ip
+    node3_ip = module.pri_mon.private_ip
   })
 }
