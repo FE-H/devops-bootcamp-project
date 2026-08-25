96c963ce61ff50228106a9a1c9dd2c564de318f5
Ho Fook Eng <hofookeng.uca@gmail.com>
2026-08-25 23:20:43 +0800

    Initialise Terraform (#1)
    * feat: EC2 initial setup
    
    * feat: Terraform outputs setup
    
    * feat: Network initial setup
    
    * feat: Terraform Providers initial setup & S3 configuration
    
    * feat: Security Group & ingress/egress rule initial setup
    
    * feat: init shell scripts
    
    * feat: setup .gitignore
    
    * fix: incorrect module reference for pub-web
    
    * fix: incorrect CIDR for VPC
    
    * feat: Terraform init

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+output "pub-web" {
+  value = module.pub-web.public_ip
+}
+
+output "ssm_pub-web" {
+  value = "aws ssm start-session --target ${module.pub-web.id}"
+}
+
+#TO-DO : check if when apply able to SSM into private servers - DONE
+output "pri-ctr" {
+  value = module.pri-ctr.public_ip
+}
+
+output "ssm_pri-ctr" {
+  value = "aws ssm start-session --target ${module.pri-ctr.id}"
+}
+
+#TO-DO : check if when apply able to SSM into private servers - DONE
+output "pri-mon" {
+  value = module.pri-mon.public_ip
+}
+
+output "ssm_pri-mon" {
+  value = "aws ssm start-session --target ${module.pri-mon.id}"
+}
