546ea8269a3c4c52b0a888af41a73ae5b7e2226b
Ho Fook Eng <hofookeng.uca@gmail.com>
2026-08-26 22:57:14 +0800

    Fixed ansible inventory (#14)
    * fix: inventory module reference
    
    * fix: Terraform Template location
    
    * fix: Terraform Template location

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -3,6 +3,6 @@ resource "local_file" "inventory" {
   content = templatefile("inventory.ini.tftpl", {
     node1_ip = module.pub-web.public_ip
     node2_ip = module.pri-ctr.private_ip
-    node3_ip = module.pri_mon.private_ip
+    node3_ip = module.pri-mon.private_ip
   })
 }
