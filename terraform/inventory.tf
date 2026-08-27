052d0afb561efe8e300c1a94a47a19b8ae3beb9a
Ho <hofookeng.uca@gmail.com>
2026-08-27 23:37:41 +0800

    fix: added S3 bucket and region to automate inventory.ini generation

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,8 +1,10 @@
 resource "local_file" "inventory" {
   filename = "../ansible/inventory.ini"
   content = templatefile("inventory.ini.tftpl", {
-    node1_ip = module.pub-web.public_ip
-    node2_ip = module.pri-ctr.private_ip
-    node3_ip = module.pri-mon.private_ip
+    node1_id = module.pub-web.id
+    node2_id = module.pri-ctr.id
+    node3_id = module.pri-mon.id
+    region   = "ap-southeast-1"
+    bucket_id= module.s3_bucket.s3_bucket_id
   })
 }
