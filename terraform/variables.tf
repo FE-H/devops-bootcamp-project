36f70725343315c0957bb39416d65b0e7750bf96
Ho <hofookeng.uca@gmail.com>
2026-08-28 19:40:44 +0800

    feat: added cloudflare variables to handle automatic DNS update for pub-web

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "cloudflare_api_token"{
+  type = string
+  description = "API token for CF"
+  sensitive = true
+}
+
+variable "cloudflare_zone_id"{
+    type = string
+    description = "Zone ID for CF"
+}
+
+variable "cloudflare_record" {
+    type = string
+    description = "The subdomain or root name for the record"
+}
